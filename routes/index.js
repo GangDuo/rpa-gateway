ba07255f91054fbba1b0d17ea694aa9aa5f96569
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-04-26 16:59:44 +0900

    Windows文字化け対策

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var Iconv = require('iconv').Iconv;
 var util = require('util');
 var childProcess = require('child_process');
 var exec = util.promisify(childProcess.exec);
@@ -13,10 +14,10 @@ router.ws('/', function(ws, req) {
   ws.on('message', function(msg) {
     ws.send(JSON.stringify({data: '処理を開始しました！'}));
 
-    exec(`nslookup yahoo.co.jp`)
+    const iconv = new Iconv('SHIFT_JIS', 'UTF-8')
+    exec(`nslookup yahoo.co.jp`, {encoding: 'Shift_JIS'})
     .then(({stdout, stderr}) => {
-      console.log(stdout);
-      ws.send(JSON.stringify({data: stdout}));
+      ws.send(JSON.stringify({data: iconv.convert(stdout).toString()}));
     })
     .catch(err => {
       console.log(err)
