a2389e351e41b077cfa450352b76f8bc98b09cf0
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-05-12 15:49:09 +0900

    ユーザーに変換したCSVファイルをダウンロードさせる。#4

--- routes/obc/mv.js
+++ routes/obc/mv.js
@@ -1,19 +1,17 @@
+var fs = require("fs");
+var temp = require('temp');
+var path = require('path');
 var Iconv = require('iconv').Iconv;
 var util = require('util');
 var childProcess = require('child_process');
 var exec = util.promisify(childProcess.exec);
+const readFile = util.promisify(fs.readFile);
 var express = require('express');
 var router = express.Router();
 
 const CACHE_DIR = process.env.CACHE_DIR_OBC_MV
 const WORK_DIR = process.env.RPA_APP_HOME
 const BIN = process.env.BIN_OBC_MV
-const result = process.env.OUT_OBC_MV
-const cmds = [
-  `pushd "${WORK_DIR}"&${BIN} /cmd "import;${CACHE_DIR}"`,
-  `pushd "${WORK_DIR}"&${BIN} /cmd convert;`,
-  `pushd "${WORK_DIR}"&${BIN} /cmd "export;${result}"`,
-];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,6 +22,12 @@ router.ws('/', function(ws, req) {
   ws.on('message', async function(msg) {
     ws.send(JSON.stringify({data: '処理を開始しました！'}));
 
+    const filepath = temp.path({suffix: '.csv'}).replace(/\.(?=\w+\.csv)/, '_');
+    const cmds = [
+      `pushd "${WORK_DIR}"&${BIN} /cmd "import;${CACHE_DIR}"`,
+      `pushd "${WORK_DIR}"&${BIN} /cmd convert;`,
+      `pushd "${WORK_DIR}"&${BIN} /cmd "export;${filepath}"`,
+    ];
     const iconv = new Iconv('SHIFT_JIS', 'UTF-8')
     try {
       for (let i = 0; i < cmds.length; i++) {
@@ -37,6 +41,8 @@ router.ws('/', function(ws, req) {
           console.log(iconv.convert(stderr).toString())
         }
       }
+      let base64 = await readFile(filepath, {encoding: "base64"})
+      ws.send(JSON.stringify({data: `<a download="${path.basename(filepath)}" href="data:application/octet-stream;base64,${base64}">ダウンロード</a>`}));
     } catch (err) {
       console.log(err)
     } finally {
