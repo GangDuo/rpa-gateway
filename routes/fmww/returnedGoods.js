46ff2bf890a4e4a5930679994e1bac2537139584
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-05-16 17:31:02 +0900

    websocketで双方向通信を行う。#6

--- routes/fmww/returnedGoods.js
+++ routes/fmww/returnedGoods.js
@@ -5,4 +5,20 @@ router.get('/', function(req, res, next) {
   res.render('fmww/returnedGoods/index', { title: '仕入返品' });
 });
 
+router.ws('/', function(ws, req) {
+  ws.on('message', async function(msg) {
+    try {
+      // TODO: 返品データを取得
+      await new Promise(resolve => {
+        setTimeout(resolve, 3000)
+      })
+    } catch (error) {   
+      console.log(error)   
+    } finally {
+      ws.send(JSON.stringify({data: '終了！'}));
+      ws.send(JSON.stringify({data: ''}));
+    }
+  });
+});
+
 module.exports = router;
