761bae21632914e662cd4d126553ae3e7a02bcaa
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-05-17 13:30:33 +0900

    移動データエクスポートをクラス抽出。#6

--- routes/obc/mv.js
+++ routes/obc/mv.js
@@ -14,7 +14,7 @@ const readdir = util.promisify(fs.readdir);
 const rename = util.promisify(fs.rename);
 var express = require('express');
 var router = express.Router();
-const { FmClient, MovementExport, Between } = require('fmww-library');
+const MovementExporter = require('../components/MovementExporter');
 const dayjs = require('dayjs')
 
 const WORK_DIR = process.env.RPA_APP_HOME
@@ -31,10 +31,14 @@ router.ws('/', function(ws, req) {
 
     const filepath = temp.path({suffix: '.csv'}).replace(/\.(?=\w+\.csv)/, '_');
     const tmpdir = path.join(os.tmpdir(), Math.random().toString(36).substring(2));
+    const me = new MovementExporter;
     console.log(tmpdir)
     try {
       await mkdir(tmpdir);
-      await download(tmpdir);
+      await me.export(tmpdir, {
+        beginDate: dayjs().subtract(1, "month").startOf('month').format('YYYY-MM-DD'),
+        endDate: dayjs().subtract(1, "month").endOf('month').format('YYYY-MM-DD'),
+      });
       await rmPeriodInFilename(tmpdir);
       await execCmds([
         `pushd "${WORK_DIR}"&${BIN} /cmd "import;${tmpdir}"`,
@@ -51,27 +55,6 @@ router.ws('/', function(ws, req) {
     }
   });
 });
-
-async function download(dir) {
-  const client = new FmClient()
-  await client.open(process.env.FMWW_SIGN_IN_URL)
-  .signIn({
-    FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
-    FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
-    FMWW_SECRET_ACCESS_KEY : process.env.FMWW_SECRET_ACCESS_KEY,
-    FMWW_PASSWORD          : process.env.FMWW_PASSWORD
-  })
-  .createAbility(MovementExport);
-
-  await client.export({
-    directoryToSaveFile: dir,
-    between: new Between(
-      dayjs().subtract(1, "month").startOf('month').format('YYYY-MM-DD'),
-      dayjs().subtract(1, "month").endOf('month').format('YYYY-MM-DD')
-    ),
-  })
-  await client.quit()
-}
 
 async function execCmds(cmds) {
   const iconv = new Iconv('SHIFT_JIS', 'UTF-8')
