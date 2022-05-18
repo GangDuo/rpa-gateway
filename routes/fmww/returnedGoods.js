198292dc5933a9198b4329bc3900248aba4a47b4
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-05-18 10:51:19 +0900

    変換後の返品データを基幹システムに登録する。#6

--- routes/fmww/returnedGoods.js
+++ routes/fmww/returnedGoods.js
@@ -3,9 +3,11 @@ var path = require('path');
 var util = require('util');
 const dayjs = require('dayjs');
 var express = require('express');
+const { FmClient, PurchasingAsBatch } = require('fmww-library');
 var router = express.Router();
 const mkdir = util.promisify(fs.mkdir);
 const readdir = util.promisify(fs.readdir);
+const stat = util.promisify(fs.stat);
 
 const MovementExporter = require('../components/MovementExporter');
 const Helpers = require('../components/Helpers');
@@ -42,13 +44,42 @@ router.ws('/', function(ws, req) {
         `pushd "${WORK_DIR}"&${BIN} /cmd "export;${filepath}"`,
         `pushd "${WORK_DIR}"&${BIN} /cmd save;`,
       ]);
+
+      const stats = await stat(filepath);
+      if(stats.size === 0) {
+        ws.send(JSON.stringify({data: '返品データがありません。'}));
+      } else {
+        const statusText = await upload(filepath);
+        if(statusText) {
+          ws.send(JSON.stringify({data: statusText}));
+        }  
+      }
     } catch (error) {   
       console.log(error)   
     } finally {
-      ws.send(JSON.stringify({data: '終了！'}));
       ws.send(JSON.stringify({data: ''}));
     }
   });
 });
+
+async function upload(filepath) {
+  console.log(`upload: ${filepath}`)
+
+  const client = new FmClient()
+  await client
+    .open(process.env.FMWW_SIGN_IN_URL)
+    .signIn({
+      FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
+      FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
+      FMWW_SECRET_ACCESS_KEY : process.env.FMWW_SECRET_ACCESS_KEY,
+      FMWW_PASSWORD          : process.env.FMWW_PASSWORD
+    })
+    .createAbility(PurchasingAsBatch)
+  const response = await client.create({
+    filePath: filepath
+  })
+  await client.quit();
+  return response.statusText;
+}
 
 module.exports = router;
