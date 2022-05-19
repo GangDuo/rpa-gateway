2cd79cefb319a266b05cbda202729dbaa9129c3e
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-05-19 15:57:32 +0900

    エンドポイント追加。#8

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var obcMvRouter = require('./routes/obc/mv');
 var returnedGoodsRouter = require('./routes/fmww/returnedGoods');
+var fcInvoiceRouter = require('./routes/invoice/fc');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/obc/mv', obcMvRouter);
 app.use('/fmww/returnedGoods', returnedGoodsRouter);
+app.use('/invoice/fc', fcInvoiceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
