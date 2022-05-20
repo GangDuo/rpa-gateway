8db8eb61885696acd43a2ee99aeb6c6754475edb
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-05-20 11:35:13 +0900

    1ヶ月分の仕入実績データを２回に分けて取得する。#8

--- routes/invoice/fc.js
+++ routes/invoice/fc.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var util = require('util');
+const dayjs = require('dayjs');
 var express = require('express');
 var router = express.Router();
 const mkdir = util.promisify(fs.mkdir);
@@ -15,32 +16,45 @@ router.get('/', function(req, res, next) {
 
 router.ws('/', function(ws, req) {
   ws.on('message', async function(msg) {
+    const tmpl = 'YYYY年MM月DD日'
+    var d1 = dayjs().subtract(1, "month").startOf('month');
+    var tasks = [{
+      begin: d1.format(tmpl),
+      end: d1.add(14, "day").format(tmpl)
+    }, {
+      begin: d1.add(15, "day").format(tmpl),
+      end: d1.endOf('month').format(tmpl)
+    }]
+
     try {
       const tmpdir = Helpers.tmpdir();
       console.log(tmpdir)
       await mkdir(tmpdir);
 
       // 基幹システムから仕入実績データを取得する。
-      const client = new FmClient()
-      await client
-        .open(process.env.FMWW_SIGN_IN_URL)
-        .signIn({
-          FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
-          FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
-          FMWW_SECRET_ACCESS_KEY : process.env.FMWW_SECRET_ACCESS_KEY,
-          FMWW_PASSWORD          : process.env.FMWW_PASSWORD
+      for (let i = 0; i < tasks.length; i++) {
+        const task = tasks[i];
+
+        const client = new FmClient()
+        await client
+          .open(process.env.FMWW_SIGN_IN_URL)
+          .signIn({
+            FMWW_ACCESS_KEY_ID     : process.env.FMWW_ACCESS_KEY_ID,
+            FMWW_USER_NAME         : process.env.FMWW_USER_NAME,
+            FMWW_SECRET_ACCESS_KEY : process.env.FMWW_SECRET_ACCESS_KEY,
+            FMWW_PASSWORD          : process.env.FMWW_PASSWORD
+          })
+          .createAbility(PurchaseHistory)
+        const response = await client.search({
+          directoryToSaveFile: tmpdir,
+          span: {
+            ...task
+          }
         })
-        .createAbility(PurchaseHistory)
-      const response = await client.search({
-        directoryToSaveFile: tmpdir,
-        span: {
-          begin: '2022年03月01日',
-          end: '2022年03月01日'
+        await client.quit()
+        if(response.statusText) {
+          ws.send(JSON.stringify({data: response.statusText}));
         }
-      })
-      await client.quit()
-      if(response.statusText) {
-        ws.send(JSON.stringify({data: response.statusText}));
       }
 
       // 勘定系システムのデータソースとしてテキストファイルを出力する。
