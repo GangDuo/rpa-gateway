421db338447f12e083e6ff717e45f3c0bc28ec68
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-06-29 14:23:29 +0900

    一時ディレクトリに廃棄データをダウンロードする。#19

--- routes/fmww/waste.js
+++ routes/fmww/waste.js
@@ -1,5 +1,11 @@
+var fs = require("fs");
+var util = require('util');
 var express = require('express');
 var router = express.Router();
+const mkdir = util.promisify(fs.mkdir);
+
+const MovementExporter = require('../components/MovementExporter');
+const Helpers = require('../components/Helpers');
 
 router.get('/', function(req, res, next) {
   res.render('fmww/waste/index', { title: '廃棄金額' });
@@ -8,7 +14,17 @@ router.get('/', function(req, res, next) {
 router.ws('/', function(ws, req) {
   ws.on('message', async function(msg) {
     try {
-      await new Promise(resolve => setTimeout(resolve, 3000));
+      const me = new MovementExporter;
+      const tmpdir = Helpers.tmpdir();
+      console.log(tmpdir)
+
+      await mkdir(tmpdir);
+
+      await me.export(tmpdir, {
+        beginDate: '2022-06-01',
+        endDate: '2022-06-30',
+        receivers: ['8000'],
+      });
     } catch (error) {   
       console.log(error)   
     } finally {
